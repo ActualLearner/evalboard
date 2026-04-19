7ff46755482781c68800d3aaed271601b77c806b
ActualLearner <abdellahshafi7@gmail.com>
2026-04-19 07:00:33 +0300

    setup pythondotenv to load environment variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
